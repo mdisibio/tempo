402229a103b2ec3cec4718b0ae4e336030ec6cd7
Martin Disibio <mdisibio@gmail.com>
2026-05-08 07:39:21 -0400

    Instead make httpfinal clone the response like the grpc paths

--- modules/frontend/combiner/trace_by_id_v2.go
+++ modules/frontend/combiner/trace_by_id_v2.go
@@ -15,20 +15,12 @@ func NewTypedTraceByIDV2(maxBytes int, marshalingFormat api.MarshallingFormat, t
 func NewTraceByIDV2(maxBytes int, marshalingFormat api.MarshallingFormat, traceRedactor TraceRedactor) Combiner {
 	var (
 		partialTrace    bool
-		finalized       bool
 		combiner        = trace.NewCombiner(maxBytes, true)
 		metricsCombiner = NewTraceByIDMetricsCombiner()
 	)
 
 	gc := &genericCombiner[*tempopb.TraceByIDResponse]{
 		combine: func(partial *tempopb.TraceByIDResponse, _ *tempopb.TraceByIDResponse, pipelineResp PipelineResponse) error {
-			// If we are finalized and already returning a response
-			// then don't modify anything.
-			// This is more efficient than cloning the response before returning it.
-			if finalized {
-				return nil
-			}
-
 			if partial.Status == tempopb.PartialStatus_PARTIAL {
 				partialTrace = true
 			}
@@ -61,10 +53,6 @@ func NewTraceByIDV2(maxBytes int, marshalingFormat api.MarshallingFormat, traceR
 				resp.Message = fmt.Sprintf("Trace exceeds maximum size of %d bytes, a partial trace is returned", maxBytes)
 			}
 
-			// There is an issue here where after we return the proto response here,
-			// more partial results could be received (due to jobs not cancelling immediately).
-			// To prevent that we signal that we are finalized and accept no more responses after.
-			finalized = true
 			return resp, nil
 		},
 		new:     func() *tempopb.TraceByIDResponse { return &tempopb.TraceByIDResponse{} },
